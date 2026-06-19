c327ef0052dd3fce2f96fabdfa1242dfec1b94d1
medenigklp-crypto <medeni.gklp@gmail.com>
2026-06-19 13:44:04 +0300

    Refactor service worker event listeners

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,6 @@
-const CACHE_NAME = "derspaneli-v3";
-
-self.addEventListener("install", event => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
-    )
-  );
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
