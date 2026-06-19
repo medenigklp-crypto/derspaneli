46134ac62785a4e8e07301cfc8f1ddc9c61065b9
medenigklp-crypto <medeni.gklp@gmail.com>
2026-06-19 11:08:32 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "derspanel-v2";
+
+const urlsToCache = [
+  "/derspanel/",
+  "/derspanel/index.html",
+  "/derspanel/icon-192.png",
+  "/derspanel/icon-512.png",
+  "/derspanel/icon-180.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      return caches.match("/derspanel/index.html");
+    })
+  );
+});
